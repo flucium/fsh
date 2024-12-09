809f3d03546fc988ce3750e5ad0eeda49128e865
flucium <flucium@flucium.net>
2024-12-09 13:40:50 +0900

    refactor ./src/profile.rs
    added doc comment.

--- src/profile.rs
+++ src/profile.rs
@@ -15,6 +15,17 @@ pub const DEFAULT_PROFILE_PATH: &str = "~/.profile.fsh";
 
 pub const DEFAULT_PROFILE: &str = "";
 
+/// Reads the content of a profile file.
+///
+/// This function attempts to open and read the specified profile file. If the file does not exist
+/// or cannot be read, an error is returned.
+///
+/// # Arguments
+/// - `path`: The file path to the profile.
+///
+/// # Returns
+/// - `Ok(String)`: The content of the profile file as a string.
+/// - `Err(Error)`: An error if the file is not found or cannot be read.
 pub fn read_profile(path: &str) -> Result<String> {
     File::open(path)
         .map_err(|_| Error::new(ErrorKind::NotFound, format!("profile not found: {}", path)))
@@ -26,6 +37,18 @@ pub fn read_profile(path: &str) -> Result<String> {
         })
 }
 
+/// Writes content to a profile file.
+///
+/// This function creates or overwrites the specified profile file with the provided content. If
+/// the file cannot be created or written to, an error is returned.
+///
+/// # Arguments
+/// - `path`: The file path to the profile.
+/// - `content`: The content to write to the profile.
+///
+/// # Returns
+/// - `Ok(())`: If the write operation is successful.
+/// - `Err(Error)`: An error if the file cannot be created or written to.
 pub fn write_profile(path: &str, content: &str) -> Result<()> {
     File::create(path)
         .map_err(|_| {
@@ -41,12 +64,35 @@ pub fn write_profile(path: &str, content: &str) -> Result<()> {
         })
 }
 
+/// Appends content to an existing profile file.
+///
+/// This function reads the existing profile file, appends the provided content, and writes the
+/// updated content back to the file. If the file does not exist or cannot be written to, an error
+/// is returned.
+///
+/// # Arguments
+/// - `path`: The file path to the profile.
+/// - `content`: The content to append to the profile.
+///
+/// # Returns
+/// - `Ok(())`: If the update operation is successful.
+/// - `Err(Error)`: An error if the file cannot be read or written to.
 pub fn update_profile(path: &str, content: &str) -> Result<()> {
     let mut profile = read_profile(path)?;
     profile.push_str(content);
     write_profile(path, &profile)
 }
 
+/// Checks if a profile file exists.
+///
+/// This function checks if the specified profile file exists and is accessible.
+///
+/// # Arguments
+/// - `path`: The file path to the profile.
+///
+/// # Returns
+/// - `true`: If the file exists and can be opened.
+/// - `false`: If the file does not exist or cannot be opened.
 pub fn exists(path: &str) -> bool {
     File::open(path).is_ok()
 }
