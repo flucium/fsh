951754178f6dadcd6d51759394a7ddc964d4ab67
flucium <flucium@flucium.net>
2025-02-09 02:08:28 +0900

    add ./src/manifest.rs

---
+++ src/manifest.rs
@@ -0,0 +1,8 @@
+/// Package Name.
+pub const MANIFEST_FSH_NAME: &str = env!("CARGO_PKG_NAME");
+
+/// Package Version.
+pub const MANIFEST_FSH_VERSION: &str = env!("CARGO_PKG_VERSION");
+
+/// Authors.
+pub const MANIFEST_FSH_AUTHORS: &str = env!("CARGO_PKG_AUTHORS");
