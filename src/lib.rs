f4cf92effa30edd23d899d3c62380d6231f19c78
flucium <flucium@flucium.net>
2025-08-06 00:35:55 +0900

    add ./src/lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+#![feature(test)]
+extern crate test;
+
+pub mod lexer;
+pub mod preprocessor;
+pub mod token;
+pub mod result;
+pub mod error;
+pub mod sh_vars;
+pub mod terminal;
+pub mod builtin;
+pub mod ast;
+// pub mod process_handler;
+pub mod state;
+pub mod parser;
+pub mod manifest;
+pub mod execute;
+pub mod profile;
