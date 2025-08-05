91e492b3d850185cd5955c10ea275562ef2d891d
flucium <flucium@flucium.net>
2025-08-06 00:40:57 +0900

    add ./src/manifest.rs

---
+++ src/manifest.rs
@@ -0,0 +1,8 @@
+/// The name of the FSH package as defined in `Cargo.toml`.
+pub const MANIFEST_FSH_NAME: &str = env!("CARGO_PKG_NAME");
+
+/// The version of the FSH package as defined in `Cargo.toml`.
+pub const MANIFEST_FSH_VERSION: &str = env!("CARGO_PKG_VERSION");
+
+/// The authors of the FSH package as defined in `Cargo.toml`.
+pub const MANIFEST_FSH_AUTHORS: &str = env!("CARGO_PKG_AUTHORS");
