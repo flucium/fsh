756348626ac78c45708aa6ad4eca4e484adfeef1
flucium <flucium@flucium.net>
2025-08-07 06:22:29 +0900

    add ./src/builtin.rs builtin/common.rs builtin.unix.rs

---
+++ src/builtin.rs
@@ -0,0 +1,9 @@
+pub mod common;
+
+pub use common::*;
+
+// #[cfg(any(target_os = "linux", target_os = "macos"))]
+// pub mod unix;
+
+// #[cfg(any(target_os = "linux", target_os = "macos"))]
+// pub use unix::*;
