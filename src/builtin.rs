597b4ec59645ca3cf61223ec9e69b249e033c143
flucium <flucium@flucium.net>
2026-01-01 23:44:18 +0900

    cleanup ./src

--- src/builtin.rs
+++
@@ -1,9 +0,0 @@
-pub mod common;
-
-pub use common::*;
-
-// #[cfg(any(target_os = "linux", target_os = "macos"))]
-// pub mod unix;
-
-// #[cfg(any(target_os = "linux", target_os = "macos"))]
-// pub use unix::*;
